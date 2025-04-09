062bef552803fa2c44b47cce0ac1e0b68bf9c3ea
nyxx <leila.wilde@laplateforme.io>
2025-04-09 15:39:55 +0100

    added job06

---
+++ job06/job06_asynch/job06_asynch.ino
@@ -0,0 +1,18 @@
+// Flash 2 LEDs asynchronously
+//
+
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(12, HIGH);
+  digitalWrite(13, LOW);
+  delay(500); // Wait for 500 millisecond(s)
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH);
+  delay(500); // Wait for 500 millisecond(s)
+}
