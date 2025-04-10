ea7c0128e041a6f6e5006168b11b3da12d0ae8c7
nyxx <leila.wilde@laplateforme.io>
2025-04-10 17:15:43 +0100

    refactored and commented job10 code

--- job10/job10/job10.ino
+++ job10/job10/job10.ino
@@ -1,16 +1,23 @@
-const int analog_ip = A0;
+const int POTENTIOMETER = A0;
 const int LED_RED = 11;
 const int LED_GREEN = 10;
 
-int  inputVal = 0;
+int reading = 0; // store potentiometer reading
 
 void setup() {
+  
   pinMode (LED_RED, OUTPUT);
   pinMode (LED_GREEN,  OUTPUT);
+  
 }
 
 void loop() {
-  inputVal = analogRead(analog_ip);
-  analogWrite  (LED_RED, inputVal/4);
-  analogWrite (LED_GREEN, (1023-inputVal)/4);
+  
+  reading = analogRead(POTENTIOMETER); // read value from potentiometer
+  /* potentiometer reading must be divided by 4
+  to scale it down from a range of 0-1023 to 0-255,
+  the range used by analogWrite() */
+  analogWrite(LED_RED, reading/4); // set brightness of red LED
+  analogWrite(LED_GREEN, (1023-reading)/4); // set the brightness of green LED
+
 }
